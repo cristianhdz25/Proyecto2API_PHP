3e032ec221dcbe0bcbdf450886673f77fde72e38
cristianhdz25 <cristian.hernandez@ucr.ac.cr>
2024-05-29 17:00:58 -0600

    bd

--- db_proyecto2lenguajes.sql
+++ db_proyecto2lenguajes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3307
--- Tiempo de generación: 27-05-2024 a las 23:25:01
+-- Tiempo de generación: 30-05-2024 a las 01:00:22
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.0.25
 
@@ -61,22 +61,23 @@ INSERT INTO `categoria` (`id_Categoria`, `nombre`) VALUES
 
 CREATE TABLE `cupon` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
   `imgUrl` varchar(500) NOT NULL,
   `ubicacion` varchar(800) NOT NULL,
   `precioBase` double NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `id_Categoria` int(11) NOT NULL,
-  `id_Empresa` int(11) NOT NULL,
-  `id_Promocion` int(11) NOT NULL
+  `id_Empresa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `cupon`
 --
 
-INSERT INTO `cupon` (`id`, `nombre`, `imgUrl`, `ubicacion`, `precioBase`, `activo`, `id_Categoria`, `id_Empresa`, `id_Promocion`) VALUES
-(1, 'Cupon Para Coca Colas', 'img.png', 'SJ Costa Rica', 2000, 1, 1, 1, 1);
+INSERT INTO `cupon` (`id`, `nombre`, `imgUrl`, `ubicacion`, `precioBase`, `activo`, `id_Categoria`, `id_Empresa`) VALUES
+(1, 'Cristian', './img/rick-morty.png', 'Costa Rica', 20000, 0, 1, 1),
+(2, 'Descuento 15%', './img/thumb-1920-1334857.png', 'Costa Rica', 20000, 1, 1, 11),
+(3, 'Cristian', './img/coctel-sexo-en-la-playa-sex.jpg', 'Costa Rica', 20000, 1, 1, 11);
 
 -- --------------------------------------------------------
 
@@ -102,16 +103,17 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`id`, `correo`, `contrasenna`, `nombre`, `direccionFisica`, `cedula`, `fechaCreacion`, `telefono`, `primeraVez`, `activo`) VALUES
-(1, 'losreyes2504@gmail.com', '1324', 'Dos Pinos 1', 'Los Alpes', '11-2222-2555', '2005-12-27', '78965874', 0, 0),
+(1, 'losreyes2504@gmail.com', '1324', 'Dos Pinos 1', 'Los Alpes', '11-2222-2555', '2005-12-25', '78965874', 0, 0),
 (2, 'losreyes2504@gmail.com', '1234', 'Cristian Hernandez', 'Juan Viñas Costa Rica', '22-1425-0144', '2024-04-17', '72810534', 0, 0),
 (3, 'majobm@gmail.com', '1234', 'Cristian', 'Juan Viñas', '22-1425-0144', '2023-08-17', '72810534', 0, 0),
 (4, 'losreyes2504@gmail.com', '1234', 'Dos Pinos 3', 'Juan Viñas', '22-1425-0144', '2024-04-23', '72810534', 0, 0),
-(5, 'losreyes2504@gmail.com', '1234', 'Dos Pinos', 'Juan Viñas', '22-1425-0144', '2024-04-22', '72810535', 0, 0),
-(6, 'majobm@gmail.com', '1234', 'Probando', 'Juan Viñas', '22-1425-0144', '2024-04-12', '72810534', 0, 0),
+(5, 'losreyes2504@gmail.com', '1234', 'Dos Pinos', 'Juan Viñas', '22-1425-0144', '2024-04-21', '72810535', 0, 1),
+(6, 'majobm@gmail.com', '1234', 'Probando', 'Juan Viñas', '22-1425-0144', '2024-04-11', '72810534', 0, 1),
 (7, 'joseant2302@gmail.com', '13', 'Cristian 2', 'Juan Viñas', '11-2225-1457', '2018-04-16', '72849578', 1, 0),
 (8, 'rolo@gmail.com', '134', 'Prueba', 'Coronado', '22-1425-0144', '2024-05-01', '72812547', 1, 0),
 (9, 'majobm@gmail.com', '1234', 'Cristian', 'Coronado', '22-1425-0144', '2024-04-16', '78965874', 1, 0),
-(10, 'yordi@gmail.com', '134', 'Yordi', 'Juan Viñas', '11-2222-2555', '2024-05-03', '12587487', 1, 0);
+(10, 'yordi@gmail.com', '134', 'Yordi', 'Juan Viñas', '11-2222-2555', '2024-05-03', '12587487', 1, 0),
+(11, 'pepsi@gmail.com', '1234', 'Pepsi', 'Coronado', '22-1425-0144', '2024-05-05', '72810534', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -121,18 +123,13 @@ INSERT INTO `empresa` (`id`, `correo`, `contrasenna`, `nombre`, `direccionFisica
 
 CREATE TABLE `promocion` (
   `id_Promocion` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
   `porcentaje` double NOT NULL,
   `fechaInicio` date NOT NULL,
   `fechaVencimiento` date NOT NULL,
+  `id_Cupon` int(11) NOT NULL,
   `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-
---
--- Volcado de datos para la tabla `promocion`
---
-
-INSERT INTO `promocion` (`id_Promocion`, `porcentaje`, `fechaInicio`, `fechaVencimiento`, `activo`) VALUES
-(1, 0.15, '2024-03-15', '2024-05-06', 1);
 
 --
 -- Índices para tablas volcadas
@@ -154,10 +151,7 @@ ALTER TABLE `categoria`
 -- Indices de la tabla `cupon`
 --
 ALTER TABLE `cupon`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cuponEmpresa` (`id_Empresa`),
-  ADD KEY `cuponCategoria` (`id_Categoria`),
-  ADD KEY `cuponPromocion` (`id_Promocion`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `empresa`
@@ -191,31 +185,19 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `cupon`
 --
 ALTER TABLE `cupon`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `promocion`
 --
 ALTER TABLE `promocion`
-  MODIFY `id_Promocion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `cupon`
---
-ALTER TABLE `cupon`
-  ADD CONSTRAINT `cuponCategoria` FOREIGN KEY (`id_Categoria`) REFERENCES `categoria` (`id_Categoria`),
-  ADD CONSTRAINT `cuponEmpresa` FOREIGN KEY (`id_Empresa`) REFERENCES `empresa` (`id`),
-  ADD CONSTRAINT `cuponPromocion` FOREIGN KEY (`id_Promocion`) REFERENCES `promocion` (`id_Promocion`);
+  MODIFY `id_Promocion` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
